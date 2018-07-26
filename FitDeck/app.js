62656077190cd44adf89067444e514728944e563
waldalvajr <waldalvajr@gmail.com>
2018-07-25 20:23:40 -0400

    randomize list

--- FitDeck/app.js
+++ FitDeck/app.js
@@ -1,20 +1,56 @@
 const ol = document.querySelector('#workoutList');
-const lis = document.querySelectorAll('#workoutList li');
-const btn = document.querySelector('#shuffle');
+let ilArray = ol.children;
+let randomItem;
+const shuffleBtn = document.querySelector('#shuffle');
 
-//event Listener
-document.getElementById("shuffle").addEventListener("click",
-function(e){
+//turn the HTML collection into an array
+ilArray = []
+  .slice
+  .call(ilArray);
 
-for (var i = ol.children.length; i >= 0; i--) {
-    ol.appendChild(ol.children[Math.random() * i | 0]);
+val = ilArray;
+console.log(val, val.length, val[0]);
+
+
+
+let randomizeList = function () {
+  console.log('kevin');
+  let randomArray = [];
+  //iterate through the enitre ilArray until there are no items left
+  console.log(ilArray.length);
+  for (let i = ilArray.length; i > 0; i--) {
+    console.log('fab');
+    //pull out a random item
+    let randoNumber = Math.floor(Math.random() * ilArray.length);
+    console.log(randoNumber);
+    //removes the item and returns it as an array of 1
+    randomItem = ilArray.splice(randoNumber, 1);
+    console.log(randomItem[0]);
+    console.log(ilArray);
+
+    //add the removed element (which comes as an array) to the randomArray
+    randomArray = randomArray.concat(randomItem);
+    console.log(randomArray);
   }
+  ilArray = randomArray;
+  return randomArray;
+}
 
-  for(var i=12; li=lis[i]; i--) {
-    li.style.opacity = '0.3';
+let refreshList = function () {
+  console.log('waldo');
+  //remove all the child li elements from the original ol
+  let randomArray = randomizeList();
+  
+  // while (ol.firstChild) {
+  //   ol
+  //     .firstChild
+  //     .remove();
+  // }
+
+  //add the randomized list
+  for (let i = 0; i < randomArray.length; i++) {
+    ol.appendChild(randomArray[i]);
   }
-      // li.parentNode.removeChild(li);
+}
 
-
-btn.value = 'Reset';
-});
+shuffleBtn.addEventListener('click', refreshList);
